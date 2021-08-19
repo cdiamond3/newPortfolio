df532bf6e62f0e4032e37e6993dddf17479f599d
Chris Diamond <diamond.chris.3@gmail.com>
2021-08-19 16:57:47 -0600

    Got sass working on mac

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/contact/Contact';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import TopBar from './components/topbar/TopBar';
+import Works from './components/works/Works';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TopBar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Testimonials />
+        <Works />
+        <Contact />
+      </div>
     </div>
   );
 }
---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,11 @@
+import "./topbar.scss"
+
+import React from 'react'
+
+export default function TopBar() {
+    return (
+        <div className="topbar">
+            Test
+        </div>
+    )
+}
