b7e6e8462045ab81215892fa39b8e6575f313082
Chris Diamond <diamond.chris.3@gmail.com>
2021-08-19 22:25:44 -0600

    ready for intro animations

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -3,11 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro" >
-            <div className="left"></div>
-            <div className="imgContainer">
-                
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="../man.png" alt="selfie" />
+                </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Chris Diamond</h1>
+                    <h3>Freelance <span> Designer </span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="../down.png" alt=" " />
+                </a>
+            </div>
         </div>
     )
 }
