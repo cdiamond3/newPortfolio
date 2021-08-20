03e765a622cfcd6b427ffbc8254c2629dbe1bf21
Chris Diamond <diamond.chris.3@gmail.com>
2021-08-19 21:24:09 -0600

    ready for images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
 import TopBar from './components/topbar/TopBar';
 import Works from './components/works/Works';
+import Menu from './components/menu/Menu'
 import "./app.scss"
 import { useState } from "react"
 
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="app">
       <TopBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
